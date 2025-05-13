37a615f2a2f77c89cdeb8817aa43771e53eee50f
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2025-05-13 19:32:42 +0300

    Initial commit

---
+++ StorageService/src/test/kotlin/com/storage/storageservice/StorageServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.storage.storageservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
