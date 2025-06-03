b3aada90f17fb22b8799d996c2c7698d41ca0dcd
i-gabdrahmanov <alex.geo.kpp@icloud.com>
2025-06-03 19:35:09 +0300

    add recursive save for artifact

--- StorageService/src/main/java/com/storage/storageservice/service/impl/ArtifactServiceImpl.java
+++ StorageService/src/main/java/com/storage/storageservice/service/impl/ArtifactServiceImpl.java
@@ -1,6 +1,5 @@
 package com.storage.storageservice.service.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.storage.storageservice.dto.ArtifactDto;
 import com.storage.storageservice.model.Artifact;
 import com.storage.storageservice.repository.ArtifactRepository;
@@ -14,14 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArtifactServiceImpl implements ArtifactService {
 
     public final ArtifactRepository artifactRepository;
-    public final ObjectMapper objectMapper;
     @Override
     @Transactional
     public void addNewArtifact(ArtifactDto dto) {
+       addNewArtifactRecursive(dto, null);
+    }
+
+    private void addNewArtifactRecursive(ArtifactDto dto, Artifact parent) {
         Artifact artifact = new Artifact();
         artifact.setName(dto.getName());
         artifact.setSurname(dto.getSurname());
         artifact.setPayload(dto.getPayload());
+        if (parent != null) {
+            artifact.setParent(parent);
+        }
+        if (dto.getChildren() != null) {
+            dto.getChildren().forEach(c -> addNewArtifactRecursive(c, artifact));
+        }
         artifactRepository.save(artifact);
     }
 }
